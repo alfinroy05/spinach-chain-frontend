1bafbb20951c2d19e859a48dbe1881294e868b73
alfinroy05 <alfinroy2005@gmail.com>
2026-03-19 02:38:02 +0530

    done demo

--- src/pages/transporter/TransporterDashboard.jsx
+++ src/pages/transporter/TransporterDashboard.jsx
@@ -5,7 +5,7 @@ import "./TransporterDashboard.css";
 import contractABI from "../../contracts/abi.json";
 
 const CONTRACT_ADDRESS = "0x826849f64E347BAA34a77360074E6569EaF0dDdd";
-const API_BASE = "process.env.REACT_APP_API_URL";
+const API_BASE = process.env.REACT_APP_API_URL;
 const SEPOLIA_CHAIN_ID = 11155111;
 
 const stateMap = [
